27146de592aa78acf00a498341080462737c7dda
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-04-04 13:20:50 -0400

    lab completed

--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
 require_relative 'config/environment'
+require_relative "models/student.rb"
 
 class App < Sinatra::Base
+  # need two controller actions –
+  # one to serve up the form,
+  # and one to process the data from the form
 
+  get '/' do
+    erb :new
+  end
+
+  post '/student' do
+    @student = Student.new(params[:student])
+
+    params[:student][:courses].each do |details|
+      Course.new(details)
+    end
+
+    @courses = Course.all
+
+    erb :student
+  end
 end
